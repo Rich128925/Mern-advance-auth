33d349a7c91034c1f0134b8e485509975a8169da
God'swill <sgodswill05@gmail.com>
2026-05-22 03:55:27 -0700

    feat: implement sign-up UI and configure Mailtrap email services for authentication flows

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -1,14 +1,19 @@
 import { MailtrapClient } from "mailtrap";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 export const mailtrapClient = new MailtrapClient({
-	endpoint: process.env.MAILTRAP_ENDPOINT,
-	token: process.env.MAILTRAP_TOKEN,
+  token: process.env.MAILTRAP_TOKEN,
+  testInboxId: Number(process.env.MAILTRAP_INBOX_ID),
 });
 
 export const sender = {
-	email: "mailtrap@demomailtrap.com",
-	name: "Burak",
+  email: "hello@example.com",
+  name: "Boluwatife",
 };
